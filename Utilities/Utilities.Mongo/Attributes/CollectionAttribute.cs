18e068629e78be539930e3371aedd31c5ff2735b
renmaobo <renmaobo@outlook.com>
2017-10-09 15:37:18 +0800

    提交CollectionAttribute

---
+++ Utilities/Utilities.Mongo/Attributes/CollectionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Mongo.Attributes
+{
+    /// <summary>
+    /// 文档集名称特性
+    /// </summary>
+    public class CollectionAttribute : Attribute
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Describe { get; set; }
+    }
+}
