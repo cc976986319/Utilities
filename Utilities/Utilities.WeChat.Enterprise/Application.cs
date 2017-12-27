6830fa9efdd5cb5b2ecf3fb30916a3de2f69c223
renmaobo <renmaobo@outlook.com>
2017-12-27 16:01:53 +0800

    提交用户操作、企业应用操作
    添加了用户操作、企业应用操作

---
+++ Utilities/Utilities.WeChat.Enterprise/Application.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.WeChat.Enterprise.Microsoft;
+
+namespace Utilities.WeChat.Enterprise
+{
+    /// <summary>
+    /// 企业应用
+    /// </summary>
+    public class Application
+    {
+        /// <summary>
+        /// 获取企业号应用
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="agentid">企业应用的id</param>
+        /// <param name="requestUrl">请求地址</param>
+        /// <remarks>该API用于获取企业号某个应用的基本信息，包括头像、昵称、帐号类型、认证类型、可见范围等信息</remarks>
+        /// <returns></returns>
+        public GetResult Get(string accessToken, int agentid, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/agent/get")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.DownloadData($"{requestUrl}?access_token={accessToken}&agentid={agentid}");
+                return bytes.ConvertTo<GetResult>();
+            }
+        }
+
+        /// <summary>
+        /// 设置企业号应用
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="body">应用信息</param>
+        /// <param name="requestUrl">请求地址</param>
+        /// <remarks>该API用于设置企业应用的选项设置信息，如：地理位置上报等。第三方服务商不能调用该接口设置授权的主页型应用。</remarks>
+        /// <returns></returns>
+        public ResponseResult Set(string accessToken, Body body, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/agent/set")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.UploadData($"{requestUrl}?access_token={accessToken}", "POST", body.ToBytes());
+                return bytes.ConvertTo<ResponseResult>();
+            }
+        }
+
+        /// <summary>
+        /// 获取应用概况列表
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="requestUrl">请求地址</param>
+        /// <remarks>该API 用于获取secret所在管理组内的应用概况，会返回管理组内应用的id及名称、头像等信息</remarks>
+        /// <returns></returns>
+        public ListResult List(string accessToken, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/agent/list")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.DownloadData($"{requestUrl}?access_token={accessToken}");
+                return bytes.ConvertTo<ListResult>();
+            }
+        }
+
+        /// <summary>
+        /// 获取结果
+        /// </summary>
+        public class GetResult : ResponseResult
+        {
+            /// <summary>
+            /// 企业应用id
+            /// </summary>
+            public string agentid { get; set; }
+
+            /// <summary>
+            /// 企业应用名称
+            /// </summary>
+            public string name { get; set; }
+
+            /// <summary>
+            /// 企业应用方形头像
+            /// </summary>
+            public string square_logo_url { get; set; }
+
+            /// <summary>
+            /// 企业应用圆形头像
+            /// </summary>
+            public string round_logo_url { get; set; }
+
+            /// <summary>
+            /// 企业应用详情
+            /// </summary>
+            public string description { get; set; }
+
+            /// <summary>
+            /// 企业应用可见范围（人员），其中包括userid和关注状态state
+            /// </summary>
+            public UserInfo allow_userinfos { get; set; }
+
+            /// <summary>
+            /// 企业应用可见范围（部门）
+            /// </summary>
+            public Party allow_partys { get; set; }
+
+            /// <summary>
+            /// 企业应用可见范围（标签）
+            /// </summary>
+            public Tags allow_tags { get; set; }
+
+            /// <summary>
+            /// 企业应用是否被禁用
+            /// </summary>
+            public int close { get; set; }
+
+            /// <summary>
+            /// 企业应用可信域名
+            /// </summary>
+            public string redirect_domain { get; set; }
+
+            /// <summary>
+            /// 企业应用是否打开地理位置上报 0：不上报；1：进入会话上报；2：持续上报
+            /// </summary>
+            public LocationFlag report_location_flag { get; set; }
+
+            /// <summary>
+            /// 是否接收用户变更通知。0：不接收；1：接收
+            /// </summary>
+            public InformType isreportuser { get; set; }
+
+            /// <summary>
+            /// 是否上报用户进入应用事件。0：不接收；1：接收
+            /// </summary>
+            public InformType isreportenter { get; set; }
+
+            /// <summary>
+            /// 应用类型。1：消息型；2：主页型
+            /// </summary>
+            public ApplicationType type { get; set; }
+
+            /// <summary>
+            /// 关联会话url
+            /// </summary>
+            public string chat_extension_url { get; set; }
+
+            /// <summary>
+            /// 用户信息
+            /// </summary>
+            public class UserInfo
+            {
+                public List<User> user { get; set; }
+
+                /// <summary>
+                /// 成员信息
+                /// </summary>
+                public class User
+                {
+                    public string userid { get; set; }
+
+                    public string status { get; set; }
+                }
+            }
+
+            /// <summary>
+            /// 部门信息
+            /// </summary>
+            public class Party
+            {
+                public List<int> partyid { get; set; }
+            }
+
+            /// <summary>
+            /// 标签信息
+            /// </summary>
+            public class Tags
+            {
+                public List<int> tagid { get; set; }
+            }
+        }
+
+        /// <summary>
+        /// 列表结果
+        /// </summary>
+        public class ListResult : ResponseResult
+        {
+            public List<Agent> agentlist { get; set; }
+
+            public class Agent
+            {
+                /// <summary>
+                /// 应用id
+                /// </summary>
+                public string agentid { get; set; }
+
+                /// <summary>
+                /// 应用名称
+                /// </summary>
+                public string name { get; set; }
+
+                /// <summary>
+                /// 方形头像url
+                /// </summary>
+                public string square_logo_url { get; set; }
+
+                /// <summary>
+                /// 圆形头像url
+                /// </summary>
+                public string round_logo_url { get; set; }
+            }
+        }
+
+        /// <summary>
+        /// 应用消息
+        /// </summary>
+        public class Body
+        {
+            /// <summary>
+            /// 企业应用的id
+            /// </summary>
+            public string agentid { get; set; }
+
+            /// <summary>
+            /// 企业应用是否打开地理位置上报 0：不上报；1：进入会话上报；2：持续上报
+            /// </summary>
+            public LocationFlag report_location_flag { get; set; }
+
+            /// <summary>
+            /// 企业应用头像的mediaid，通过多媒体接口上传图片获得mediaid，上传后会自动裁剪成方形和圆形两个头像
+            /// </summary>
+            public string logo_mediaid { get; set; }
+
+            /// <summary>
+            /// 企业应用名称
+            /// </summary>
+            public string name { get; set; }
+
+            /// <summary>
+            /// 企业应用详情
+            /// </summary>
+            public string description { get; set; }
+
+            /// <summary>
+            /// 企业应用可信域名
+            /// </summary>
+            public string redirect_domain { get; set; }
+
+            /// <summary>
+            /// 是否接收用户变更通知。0：不接收；1：接收。
+            /// </summary>
+            public InformType isreportuser { get; set; }
+
+            /// <summary>
+            /// 是否上报用户进入应用事件。0：不接收；1：接收。
+            /// </summary>
+            public InformType isreportenter { get; set; }
+
+            /// <summary>
+            /// 主页型应用url。url必须以http或者https开头。消息型应用无需该参数
+            /// </summary>
+            public string home_url { get; set; }
+
+            /// <summary>
+            /// 关联会话url。设置该字段后，企业会话"+"号将出现该应用，点击应用可直接跳转到此url，支持jsapi向当前会话发送消息。
+            /// </summary>
+            public string chat_extension_url { get; set; }
+        }
+
+        /// <summary>
+        /// 地理位置标记
+        /// </summary>
+        public enum LocationFlag : int
+        {
+            /// <summary>
+            /// 不上报
+            /// </summary>
+            NotInform = 0,
+            /// <summary>
+            /// 进入上报
+            /// </summary>
+            IntoInform = 1,
+            /// <summary>
+            /// 持续上报
+            /// </summary>
+            ContinueInform = 2
+        }
+
+        /// <summary>
+        /// 通知类型
+        /// </summary>
+        public enum InformType : int
+        {
+            /// <summary>
+            /// 不接受
+            /// </summary>
+            NotAccept = 0,
+            /// <summary>
+            /// 接受
+            /// </summary>
+            Accept = 1
+        }
+
+        /// <summary>
+        /// 应用类型
+        /// </summary>
+        public enum ApplicationType : int
+        {
+            /// <summary>
+            /// 消息型
+            /// </summary>
+            Message = 1,
+            /// <summary>
+            /// 主页型
+            /// </summary>
+            HomePage = 2
+        }
+    }
+}
---
+++ Utilities/Utilities.WeChat.Enterprise/Contacts/Member.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.WeChat.Enterprise.Microsoft;
+
+namespace Utilities.WeChat.Enterprise.Contacts
+{
+    /// <summary>
+    /// 成员
+    /// </summary>
+    public class Member
+    {
+        /// <summary>
+        /// 创建成员
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="body">成员信息</param>
+        /// <param name="requestUrl">请求路径</param>
+        /// <returns></returns>
+        public ResponseResult Create(string accessToken, MemberBody body, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/create")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.UploadData($"{requestUrl}?access_token={accessToken}", "POST", body.ToBytes());
+                return bytes.ConvertTo<ResponseResult>();
+            }
+        }
+
+        /// <summary>
+        /// 更新成员
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="body">成员信息</param>
+        /// <param name="requestUrl">请求路径</param>
+        /// <returns></returns>
+        public ResponseResult Update(string accessToken, MemberBody body, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/update")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.UploadData($"{requestUrl}?access_token={accessToken}", "POST", body.ToBytes());
+                return bytes.ConvertTo<ResponseResult>();
+            }
+        }
+
+        /// <summary>
+        /// 删除成员
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="userId">成员UserID。对应管理端的帐号</param>
+        /// <param name="requestUrl">请求地址</param>
+        /// <returns></returns>
+        public ResponseResult Delete(string accessToken, string userId, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/delete")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.DownloadData($"{requestUrl}?access_token={accessToken}&userid={userId}");
+                return bytes.ConvertTo<ResponseResult>();
+            }
+        }
+
+        /// <summary>
+        /// 批量删除成员
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="body">要删除的成员列表</param>
+        /// <param name="requestUrl">请求地址</param>
+        /// <returns></returns>
+        public ResponseResult BatchDelete(string accessToken, BatchDeleteBody body, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/batchdelete")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.UploadData($"{requestUrl}?access_token={accessToken}", "POST", body.ToBytes());
+                return bytes.ConvertTo<ResponseResult>();
+            }
+        }
+
+        /// <summary>
+        /// 获取成员
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="userId">成员UserID。对应管理端的帐号</param>
+        /// <param name="requestUrl">请求地址</param>
+        /// <returns></returns>
+        public Result Get(string accessToken, string userId, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/get")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.DownloadData($"{requestUrl}?access_token={accessToken}&userid={userId}");
+                return bytes.ConvertTo<Result>();
+            }
+        }
+
+        /// <summary>
+        /// 获取部门成员
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="departmentId">获取的部门id</param>
+        /// <param name="fetchChild">1/0：是否递归获取子部门下面的成员</param>
+        /// <param name="status">0获取全部成员，1获取已关注成员列表，2获取禁用成员列表，4获取未关注成员列表。status可叠加,未填写则默认为4</param>
+        /// <returns></returns>
+        public Result_SimpleList SimpleList(string accessToken, string departmentId, Fetch_Child fetchChild, MemberStatus[] status, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/simplelist")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.DownloadData($"{requestUrl}access_token={accessToken}&department_id={departmentId}&fetch_child={fetchChild}&status={status}");
+                return bytes.ConvertTo<Result_SimpleList>();
+            }
+        }
+
+        /// <summary>
+        /// 获取部门成员(详情)
+        /// </summary>
+        /// <param name="accessToken">调用接口凭证</param>
+        /// <param name="departmentId">获取的部门id</param>
+        /// <param name="fetchChild">1/0：是否递归获取子部门下面的成员</param>
+        /// <param name="status">0获取全部成员，1获取已关注成员列表，2获取禁用成员列表，4获取未关注成员列表。status可叠加,未填写则默认为4</param>
+        /// <returns></returns>
+        public Result_List List(string accessToken, string departmentId, Fetch_Child fetchChild, MemberStatus[] status, string requestUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/simplelist")
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                byte[] bytes = client.DownloadData($"{requestUrl}access_token={accessToken}&department_id={departmentId}&fetch_child={fetchChild}&status={status}");
+                return bytes.ConvertTo<Result_List>();
+            }
+        }
+
+        /// <summary>
+        /// 成员数据
+        /// </summary>
+        public class MemberBody
+        {
+            /// <summary>
+            /// 成员UserID。对应管理端的帐号，企业内必须唯一。不区分大小写，长度为1~64个字节
+            /// </summary>
+            public string userid { get; set; }
+
+            /// <summary>
+            /// 成员名称。长度为1~64个字节
+            /// </summary>
+            public string name { get; set; }
+
+            /// <summary>
+            /// 成员所属部门id列表,不超过20个
+            /// </summary>
+            public List<int> department { get; set; }
+
+            /// <summary>
+            /// 职位信息。长度为0~64个字节
+            /// </summary>
+            public string position { get; set; }
+
+            /// <summary>
+            /// 手机号码。企业内必须唯一，mobile/weixinid/email三者不能同时为空
+            /// </summary>
+            public string mobile { get; set; }
+
+            /// <summary>
+            /// 性别。1表示男性，2表示女性
+            /// </summary>
+            public string gender { get; set; }
+
+            /// <summary>
+            /// 邮箱。长度为0~64个字节。企业内必须唯一
+            /// </summary>
+            public string email { get; set; }
+
+            /// <summary>
+            /// 微信号。企业内必须唯一。（注意：是微信号，不是微信的名字）
+            /// </summary>
+            public string weixinid { get; set; }
+
+            /// <summary>
+            /// 启用/禁用成员。1表示启用成员，0表示禁用成员
+            /// </summary>
+            public int enable { get; set; }
+
+            /// <summary>
+            /// 成员头像的mediaid，通过多媒体接口上传图片获得的mediaid
+            /// </summary>
+            public string avatar_mediaid { get; set; }
+
+            /// <summary>
+            /// 扩展属性。扩展属性需要在WEB管理端创建后才生效，否则忽略未知属性的赋值
+            /// </summary>
+            public Extension extattr { get; set; }
+        }
+
+        /// <summary>
+        /// 扩展
+        /// </summary>
+        public class Extension
+        {
+            /// <summary>
+            /// 扩展属性集合
+            /// </summary>
+            public List<Attr> attrs { get; set; }
+
+            /// <summary>
+            /// 扩展属性
+            /// </summary>
+            public class Attr
+            {
+                /// <summary>
+                /// 名称
+                /// </summary>
+                public string name { get; set; }
+
+                /// <summary>
+                /// 值
+                /// </summary>
+                public string value { get; set; }
+            }
+        }
+
+        /// <summary>
+        /// 批量删除对象
+        /// </summary>
+        public class BatchDeleteBody
+        {
+            /// <summary>
+            /// 成员UserID列表。对应管理端的帐号。（最多支持200个）
+            /// </summary>
+            public List<string> useridlist { get; set; }
+        }
+
+        /// <summary>
+        /// 结果
+        /// </summary>
+        public class Result : ResponseResult
+        {
+            /// <summary>
+            /// 成员UserID。对应管理端的帐号
+            /// </summary>
+            public string userid { get; set; }
+
+            /// <summary>
+            /// 成员名称
+            /// </summary>
+            public string name { get; set; }
+
+            /// <summary>
+            /// 成员所属部门id列表
+            /// </summary>
+            public List<int> department { get; set; }
+
+            /// <summary>
+            /// 职位信息
+            /// </summary>
+            public string position { get; set; }
+
+            /// <summary>
+            /// 手机号码。第三方仅通讯录套件可获取
+            /// </summary>
+            public string mobile { get; set; }
+
+            /// <summary>
+            /// 性别。0表示未定义，1表示男性，2表示女性
+            /// </summary>
+            public string gender { get; set; }
+
+            /// <summary>
+            /// 邮箱。第三方仅通讯录套件可获取
+            /// </summary>
+            public string email { get; set; }
+
+            /// <summary>
+            /// 微信号
+            /// </summary>
+            public string weixinid { get; set; }
+
+            /// <summary>
+            /// 头像url。注：如果要获取小图将url最后的"/0"改成"/64"即可
+            /// </summary>
+            public string avatar { get; set; }
+
+            /// <summary>
+            /// 关注状态: 1=已关注，2=已禁用，4=未关注
+            /// </summary>
+            public int status { get; set; }
+
+            /// <summary>
+            /// 扩展属性。第三方仅通讯录套件可获取
+            /// </summary>
+            public Extension extattr { get; set; }
+        }
+
+        /// <summary>
+        /// 部门成员结果
+        /// </summary>
+        public class Result_SimpleList : ResponseResult
+        {
+            /// <summary>
+            /// 成员列表
+            /// </summary>
+            public List<User> userlist { get; set; }
+
+        }
+
+        /// <summary>
+        /// 部门成员结果(详情)
+        /// </summary>
+        public class Result_List : ResponseResult
+        {
+            /// <summary>
+            /// 成员列表
+            /// </summary>
+            public List<MemberBody> userlist { get; set; }
+        }
+
+        /// <summary>
+        /// 人员信息
+        /// </summary>
+        public class User
+        {
+            /// <summary>
+            /// 成员UserID。对应管理端的帐号
+            /// </summary>
+            public string userid { get; set; }
+
+            /// <summary>
+            /// 成员名称
+            /// </summary>
+            public string name { get; set; }
+
+            /// <summary>
+            /// 成员所属部门
+            /// </summary>
+            public List<int> department { get; set; }
+        }
+
+        /// <summary>
+        /// 成员获取
+        /// </summary>
+        public enum Fetch_Child : int
+        {
+            /// <summary>
+            /// 获取当前部门
+            /// </summary>
+            GetCurrent = 0,
+            /// <summary>
+            /// 获取全部(包含子部门)
+            /// </summary>
+            GetAll = 1
+        }
+
+        /// <summary>
+        /// 成员类型
+        /// </summary>
+        public enum MemberStatus : int
+        {
+            All = 0,
+            /// <summary>
+            /// 关注
+            /// </summary>
+            Attention = 1,
+            /// <summary>
+            /// 禁用
+            /// </summary>
+            Disabled = 2,
+            /// <summary>
+            /// 未关注
+            /// </summary>
+            NotAttention = 4
+        }
+    }
+}
